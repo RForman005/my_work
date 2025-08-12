3abb9baf2c95d139e5f3cc723c8632bb33c230a2
RForman005 <richtechfit86@gmail.com>
2025-08-12 11:07:59 -0400

    Add files via upload

---
+++ Python_day1.py
@@ -0,0 +1,7 @@
+# create a greeting
+print("Welcome to the New Band Name Generator")
+#Users hometown
+hometown = input("What city did you grow up in?\n ")
+#Users Pet name
+pet = input("What is the name of a name?\n ")
+print("Your new band name could be: " +hometown + " " + pet)
